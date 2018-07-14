3d6113232592c18f978069fcaaf369285181ac78
mrmodise <modisemorebodi@gmail.com>
2018-07-14 03:22:23 +0200

    updated styling

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -8,8 +8,10 @@ import {Component, OnInit} from '@angular/core';
 export class PostListComponent implements OnInit {
 
   posts = [
-    {title: 'Another post', content: 'tLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cupiditate fugit perspiciatis. Aliquam consectetur dictat'},
-    {title: 'Another post1', content: 'dolorem dolorum ea eos excepturi itaque, labore minus modi neque obcaecati quaerat quis suscipit veniam.'},
+    {title: 'Another post', content: 'tLorem ipsum dolor sit amet, consectetur ' +
+      'adipisicing elit. Accusantium cupiditate fugit perspiciatis. Aliquam consectetur dictat'},
+    {title: 'Another post1', content: 'dolorem dolorum ea eos excepturi itaque, ' +
+      'labore minus modi neque obcaecati quaerat quis suscipit veniam.'},
     {title: 'Another post2', content: 'testing another post2'},
     {title: 'Another post3', content: 'testing another post3'},
     {title: 'Another post4', content: 'testing another post4'}
