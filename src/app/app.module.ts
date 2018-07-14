899636aa1982c7db8ea20ec4a76e98287aff6f11
mrmodise <modisemorebodi@gmail.com>
2018-07-14 03:23:13 +0200

    added more material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { CreatePostComponent } from './components/create-post/create-post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {CreatePostComponent} from './components/create-post/create-post.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatExpansionModule, MatFormFieldModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
+import {HeaderComponent} from './components/header/header.component';
+import {PostListComponent} from './components/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreatePostComponent
+    CreatePostComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -18,9 +21,13 @@ import {FormsModule} from '@angular/forms';
     MatFormFieldModule,
     FormsModule,
     MatCardModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
