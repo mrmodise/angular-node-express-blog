e575f4977e91836d2bc8c7dc405ae0aabf1bdf27
mrmodise <modisemorebodi@gmail.com>
2018-07-15 07:36:49 +0200

    added proxying and post service

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Post} from '../models/post';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  savePost(post: Post) {
+    this.http.post('', {});
+  }
+
+  listPosts() {
+    return this.http.get<Post[]>('');
+  }
 }
